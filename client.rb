a0f458c1ad70933e7e86640cc8a8242d28099ee6
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-12 15:38:06 +0800

    finished simple server and client

--- client.rb
+++ client.rb
@@ -5,7 +5,12 @@ port = 2000
 
 s = TCPSocket.open(hostname, port)
 
+request = "requesting things..."
+s.puts(request)
+
+
 while line = s.gets
 	puts line.chop
 end
+
 s.close
--- server.rb
+++ server.rb
@@ -1,9 +1,16 @@
 require 'socket'
 
 server = TCPServer.open(2000)
+
 loop {
 	client = server.accept
 	client.puts(Time.now.ctime)
+	client.puts "Lol"
+	request = client.gets
+
+	#STDERR.puts request
+
+	client.puts request
 	client.puts "Closing the connection. Bye!"
 	client.close
 }
