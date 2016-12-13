82ef3d9d0676d13a0601e8862aebf1ef75b9fde4
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-13 13:50:37 +0800

    started POST request

--- simple_browser.rb
+++ simple_browser.rb
@@ -4,7 +4,26 @@ host = 'localhost'
 port = 2000
 path = '/index.html'
 
-request = "GET #{path} HTTP/1.0\r\n"
+
+
+puts "What type of request will you be doing? (GET/POST)"
+input = gets.chomp.upcase!
+
+if input == "GET"
+	request = "GET #{path} HTTP/1.0\r\n"
+elsif input == "POST"
+	puts "You choose POST"
+	puts "You are registering a viking to a raid..."
+	puts "What's your viking's name?"
+	viking_name = gets.chomp
+	puts "What's your email?"
+	email = gets.chomp
+	
+else
+	"Invalid input...Closing..."
+end
+
+
 
 socket = TCPSocket.open(host,port)
 
