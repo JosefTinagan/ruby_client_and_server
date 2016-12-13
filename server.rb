f2ef24714901ddd60720ccaf2f7d98f1c5fea761
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-12-13 13:42:43 +0800

    finished GET verb parsing and reading

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ server = TCPServer.open(2000)
 
 loop {
 	client = server.accept
-	client.puts(Time.now.ctime)
+	date = Time.now.ctime
 	
 	request = client.gets
 
@@ -13,18 +13,32 @@ loop {
 	path = /\/\w+\/\w+\.\w+|\/\w+\.\w+/.match(request)
 	http_version = /HTTP\/\d+\.\d+/.match(request)
 
+	file = File.join(File.dirname(__FILE__), path.to_s)
+	file_exist = File.exist?(file)
+
+
 	client.puts verb.to_s
 	client.puts path.to_s
 	client.puts http_version.to_s
+	client.puts 
 
-
+	client.puts file_exist
+	client.puts file
 	if verb.to_s == "GET"
 		client.puts "The Verb is GET"
+		if file_exist
+			size = File.new(file).size
+			content = File.read(file)
+			client.puts "#{http_version} 200 OK\r\nDate: #{date}\r\nContent-Type: text/html\r\nContent-Length: #{size}\r\n\r\n#{content}"
+		else
+			client.puts "404 Not Found, The File does not exist!"
+		end
+
 	elsif verb.to_s == "POST"
 		client.puts "The Verb is POST"
 	end
 
-	client.puts request.inspect
+	#client.puts request.inspect
 	client.puts "Closing the connection. Bye!"
 	client.close
 }
